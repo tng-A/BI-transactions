bd77e43cb2b9bbc8be496b8d98c82645c07c1973
kwanj-k <mwangikelvin23@gmail.com>
2019-09-26 10:50:50 +0300

    Prototype

---
+++ apps/common/models.py
@@ -0,0 +1,42 @@
+""" Common fields mixin """
+
+from django.db import models
+
+class CustomManager(models.Manager):
+    """
+    Custom manager so as not to return deleted objects
+    """
+
+    def get_queryset(self):
+        return super(CustomManager, self).get_queryset().filter(deleted=False)
+
+
+class CommonFieldsMixin(models.Model):
+    """
+    This contains all common object attributes
+    Every model will inherit this class to avoid repetition
+    Its abstract hence can't be instatiated
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    deleted = models.BooleanField(
+        default=False,
+        help_text="This is to make sure deletes are not actual deletes"
+    )
+    active = models.BooleanField(default=True)
+    # everything will be used to query deleted objects e.g Model.everything.all()
+    everything = models.Manager()
+    objects = CustomManager()
+
+    def delete(self, *args, **kwargs):
+        """Override delete"""
+        self.deleted = True
+        self.save()
+
+    class Meta:
+        """Define metadata options."""
+
+        ordering = ['-updated_at', '-created_at']
+        abstract = True
+        app_label = 'common'
--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,21 @@
+""" Transactions model"""
+
 from django.db import models
 
-# Create your models here.
+from apps.common.models import CommonFieldsMixin
+
+
+class Transaction(CommonFieldsMixin):
+    """ Transaction model"""
+    revenue_stream = models.CharField(max_length=255)
+    income_stream = models.CharField(max_length=255)
+    date_paid = models.CharField(max_length=255)
+    receipt_number = models.CharField(max_length=255, unique=True)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.revenue_stream
+    
+    class Meta:
+        abstract = False
+
